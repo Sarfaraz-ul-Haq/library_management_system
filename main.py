3cac118b593326eecad60461b879884b25fb4b4b
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-10-31 19:10:02 +0500

    Remove underscores from protected attributes for now

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from typing import List
 
 class Book:
     def __init__(self, book_id: int, title: str, author: str) -> None:
-        self._book_id: int = book_id
+        self.book_id: int = book_id
         self.title: str = title
         self.author: str = author
-        self._available: bool = True
+        self.available: bool = True
 
     def display_info(self) -> None:
-        print(f"Book ID: {self._book_id}")
+        print(f"Book ID: {self.book_id}")
         print(f"Title: {self.title}")
         print(f"Author: {self.author}")
-        print(f"Available: {self._available}")
+        print(f"Available: {self.available}")
 
     def display_book_id(self) -> None:
-        print(self._book_id)
+        print(self.book_id)
 
     def display_title(self) -> None:
         print(self.title)
@@ -24,14 +24,14 @@ class Book:
         print(self.author)
 
     def display_available(self) -> None:
-        print(self._available)
+        print(self.available)
 
 
 class User:
     def __init__(self, user_id: str, name: str, email: str) -> None:
-        self._user_id: str = user_id
-        self._name: str = name
-        self._email: str = email
+        self.user_id: str = user_id
+        self.name: str = name
+        self.email: str = email
 
 
 class Librarian(User):
